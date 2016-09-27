b693ab3f5586b3ebfaf0783015353dc270995f41
Karol Poplawski <karolpoplawski@gmail.com>
2016-09-27 15:04:13 +0200

    first js kodilla repository

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var a, h, triangleArea;
+
+a = prompt('Input the longest triange side value');
+h = prompt('Input the triangle height value');;
+
+triangleArea = a*h/2;
+
+console.log(triangleArea);
+
+alert('The area of the trianle is ' + triangleArea);
